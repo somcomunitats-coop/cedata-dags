5ef896e56e11e5de8bf3105b13a045dd2095244c
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2022-04-13 12:55:19 +0200

    prova sentilo

--- dags/conectabbdd/conecta.py
+++ dags/conectabbdd/conecta.py
@@ -10,3 +10,11 @@ def connecta():
     df = querytodataframe('select sum(b) as a from test_inici', ['a'], connraw)
     dataframetotable(table='test_desti', bbdd=conndwh, dataframe=df)
     return 'ok'
+
+
+def connecta_sentilo():
+    conndwh = BaseHook.get_connection('Datawarehouse').get_hook().get_sqlalchemy_engine()
+    connraw = BaseHook.get_connection('Sentilo').get_hook().get_sqlalchemy_engine()
+    df = querytodataframe('select count(*) as a from sentilo_observations;', ['a'], connraw)
+    dataframetotable(table='test_desti', bbdd=conndwh, dataframe=df)
+    return 'ok'
