0a1a17c4d8b48591770a320a6e01f2810dc342d8
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2023-03-02 08:18:03 +0100

    afegit procés moviment dades datalake datawarehouse

--- dags/dag_data_process.py
+++ dags/dag_data_process.py
@@ -13,7 +13,7 @@ def move_data_to_dwh():
 
 
 
-with DAG("dag_curve_download", start_date=datetime(2022, 1, 1), schedule_interval="0 4 * * *", catchup=False) as dag:
+with DAG("dag_data_process", start_date=datetime(2022, 1, 1), schedule_interval="0 4 * * *", catchup=False) as dag:
     move_data_to_dwh = PythonOperator(
         task_id="move_data_to_dwh",
         python_callable=move_data_to_dwh
