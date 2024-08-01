ae58e1a4ac39525d617332fe2457ea0c38bfc37b
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2024-08-01 09:23:09 +0200

    threads

--- dags/helpscoutproc/helpscout.py
+++ dags/helpscoutproc/helpscout.py
@@ -6,6 +6,8 @@ from airflow.models import Variable
 from coopdevsutils.coopdevsutils import dataframetotable, executequery, querytodataframe
 
 def download_helpscout():
+    conndwh = BaseHook.get_connection('DWH_CE').get_hook().get_sqlalchemy_engine()
+
     url = "https://api.helpscout.net/v2/oauth2/token"
 
     payload = {
@@ -42,10 +44,78 @@ def download_helpscout():
             page) + '&query=(modifiedAt:[NOW-24HOUR TO *])'
         response = requests.get(url, headers=headers).json()
         for x in response['_embedded']['conversations']:
+
             # Threads
-            # url2 = 'https://api.helpscout.net/v2/conversations/'+str(x['id'])+'/threads'
-            # response = requests.get(url2, headers=headers).json()
-            # print(response)
+            datathread = {'id': [], 'number': [], 'type': [], 'status': [], 'state': [], 'source_type': [],
+                          'source_via': []
+                , 'customerId': [], 'customerFirst': [], 'customerLast': []
+                , 'createdById': [], 'createdByType': [], 'createdByFirst': [], 'createdByLast': []
+                , 'assignedToId': [], 'assignedToType': [], 'assignedToFirst': [], 'assignedToLast': []
+                , 'createdAt': []
+                          }
+            pagethread = 1
+            urlthread = 'https://api.helpscout.net/v2/conversations/' + str(x['id']) + '/threads'
+            responsethread = requests.get(urlthread, headers=headers).json()
+            while pagethread == 1 or responsethread['page']['totalPages'] > responsethread['page']['number']:
+                for y in responsethread['_embedded']['threads']:
+                    datathread['id'].append(x['id'])
+                    datathread['number'].append(y['id'])
+                    datathread['type'].append(y['type'])
+                    datathread['status'].append(y['status'])
+                    datathread['state'].append(y['state'] if 'state' in y else "")
+                    datathread['source_type'].append(y['source']['type'])
+                    datathread['source_via'].append(y['source']['via'])
+
+                    if 'customer' in y:
+                        datathread['customerId'].append(y['customer']['id'])
+                        datathread['customerFirst'].append(y['customer']['first'])
+                        datathread['customerLast'].append(y['customer']['last'])
+                    else:
+                        datathread['customerId'].append("")
+                        datathread['customerFirst'].append("")
+                        datathread['customerLast'].append("")
+
+                    datathread['createdById'].append(y['createdBy']['id'])
+                    datathread['createdByType'].append(y['createdBy']['type'])
+                    datathread['createdByFirst'].append(y['createdBy']['first'])
+                    datathread['createdByLast'].append(y['createdBy']['last'])
+
+                    if 'assignedTo' in y:
+                        datathread['assignedToId'].append(y['assignedTo']['id'])
+                        datathread['assignedToType'].append(
+                            y['assignedTo']['type'] if 'type' in y['assignedTo'] else "")
+                        datathread['assignedToFirst'].append(y['assignedTo']['first'])
+                        datathread['assignedToLast'].append(y['assignedTo']['last'])
+                    else:
+                        datathread['assignedToId'].append("")
+                        datathread['assignedToType'].append("")
+                        datathread['assignedToFirst'].append("")
+                        datathread['assignedToLast'].append("")
+
+                    datathread['createdAt'].append(y['createdAt'])
+                pagethread = pagethread + 1
+            dfthread = pd.DataFrame(datathread)
+            dataframetotable("stg_helpscout_thread", conndwh, dfthread, schema="external", if_exists="replace")
+
+            qry = """
+                    create table if not exists external.helpscout_thread as 
+                    select *
+                    from external.stg_helpscout_thread sh;
+
+
+                    delete from external.helpscout_thread
+                    where exists (
+                        select * 
+                        from external.stg_helpscout_thread h
+                        where h.id=helpscout.id
+                    );
+
+                    insert into external.helpscout_thread
+                    select * 
+                    from external.stg_helpscout_thread h;
+                """
+            executequery(qry, conndwh)
+
 
             data['id'].append(x['id'])
             data['number'].append(x['number'])
@@ -71,9 +141,7 @@ def download_helpscout():
         page = page + 1
 
     df = pd.DataFrame(data)
-    print(df)
 
-    conndwh = BaseHook.get_connection('DWH_CE').get_hook().get_sqlalchemy_engine()
     dataframetotable("stg_helpscout", conndwh, df, schema="external", if_exists="replace")
 
     qry = """
