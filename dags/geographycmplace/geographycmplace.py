56fe4b880394a7658fada3628f3238b7f05640da
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2025-09-24 09:29:10 +0200

    geography_cm_place

--- dags/geographycmplace/geographycmplace.py
+++ dags/geographycmplace/geographycmplace.py
@@ -9,7 +9,12 @@ def calc_geography_cm_place():
     conndwh = BaseHook.get_connection('DWH_CE').get_hook().get_sqlalchemy_engine()
 
     qry = """
-        select p.id, p.lat, p.lng 
+        select p.id, p.lat, p.lng
+        ,case when isnumeric((string_to_Array(p.address_txt ,','))[1]) then 
+                case when length((string_to_Array(p.address_txt ,','))[1])=4 then '0' else '' end || (string_to_Array(p.address_txt ,','))[1]
+            when isnumeric((string_to_Array(p.address_txt ,','))[2]) then 
+                case when length((string_to_Array(p.address_txt ,','))[2])=4 then '0' else '' end || (string_to_Array(p.address_txt ,','))[2]  
+        end as cp
         from odoo_cm_place p 
         where not exists (
             select *
@@ -19,7 +24,7 @@ def calc_geography_cm_place():
     """
 
 
-    df = querytodataframe(qry, ["id", "lat", "lng"], conndwh)
+    df = querytodataframe(qry, ["id", "lat", "lng", "cp"], conndwh)
     for index, row in df.iterrows():
         lat = row["lat"].replace(',','')
         geourl = f'https://www.cartociudad.es/geocoder/api/geocoder/reverseGeocode?lon={row["lng"]}&lat={lat}'
@@ -33,3 +38,8 @@ def calc_geography_cm_place():
             dataframetotable('geography_cm_place', conndwh, df, schema="external")
         except:
             print(geourl)
+            res = [[row["id"], None, None, None,
+                    None,  row["cp"][:5]]]
+            df = pd.DataFrame(res,
+                              columns=['id_cm_place', 'municipi', 'comarca', 'provincia', 'ccaa', 'codpostal'])
+            dataframetotable('geography_cm_place', conndwh, df, schema="external")
